0f8f164ec20dd599044d74ed4e89888abfbe8cfa
Noah <183996189+ur98tr8789461@users.noreply.github.com>
2026-04-03 21:44:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
 const app = express();
 
-// middleware
-app.use(cors());
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
-  res.send("Backend is alive 🔥");
+  res.send("Server is alive 🔥");
 });
 
-// start server
-const PORT = process.env.PORT || 3000;
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Render API 👋" });
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
